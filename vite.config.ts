f0721f9eb04215b57f72617efc6156d8addcc75a
Parth Chandna <Chandnaparth64@gmail.com>
2025-07-07 22:16:34 +0530

    Hardcoded correct VITE_API_BASE_URL to fix Vercel not injecting env

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
   server: {
@@ -15,4 +14,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  define: {
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify('https://uttrakhand-tennis-association.onrender.com')
+  }
 });
